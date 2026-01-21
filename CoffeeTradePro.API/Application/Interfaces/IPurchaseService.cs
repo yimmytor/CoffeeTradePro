9a59ed0a47da3d7ca9131ef9fe4ef476a42fb8e7
Jimmy Torres <yimmytor@yahoo.es>
2026-01-21 01:08:00 -0600

    Add repository and service interfaces

---
+++ CoffeeTradePro.API/Application/Interfaces/IPurchaseService.cs
@@ -0,0 +1,9 @@
+﻿using CoffeeTradePro.API.Domain.Entities;
+
+namespace CoffeeTradePro.API.Application.Interfaces
+{
+    public interface IPurchaseService
+    {
+        Task<Purchase> CreatePurchaseAsync(Purchase purchase);
+    }
+}
---
+++ CoffeeTradePro.API/Application/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using CoffeeTradePro.API.Domain.Entities;
+
+namespace CoffeeTradePro.API.Application.Interfaces
+{
+    public interface ISupplierRepository
+    {
+        Task<Supplier> GetByIdAsync(int Id);
+        Task<IEnumerable<Supplier>> GetAllAsync();
+    }
+}
